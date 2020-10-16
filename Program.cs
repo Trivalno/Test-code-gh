adfd0ccac58c8b7a8e4ee80416c4378a98a9444e
tristan hasquenoph <trivalno@hotmail.fr>
2020-10-16 11:39:55 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test_code_gh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coucou = 3;
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine(coucou);
+            Console.ReadLine();
+            
+        }
+    }
+}
